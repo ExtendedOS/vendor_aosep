e54fa63ed2a58ce9d287b4dd0fafa0c42ca1bc90
arshaprasad <42238183+arshaprasad@users.noreply.github.com>
2019-03-16 14:13:22 +0530

    Update repopick.py

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -128,8 +128,8 @@ def fetch_query(remote_url, query):
         raise Exception('Gerrit URL should be in the form http[s]://hostname/ or ssh://[user@]host[:port]')
 
 if __name__ == '__main__':
-    # Default to scissor Gerrit
-    default_gerrit = 'https://review.scissorgzr.com'
+    # Default to AOSEP Gerrit
+    default_gerrit = 'https://review.AOSEPgzr.com'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
